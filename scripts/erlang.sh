d7cdfbef5d105de40582e7e205feaaa0934465d6
eabz <eabz@polispay.org>
2021-08-01 22:00:16 -0500

    Dep

--- scripts/erlang.sh
+++ scripts/erlang.sh
@@ -17,7 +17,7 @@ fi
 
 function install() {
   echo "Installing Erlang..."
-  apt install zip -y
+  apt install zip build-essential -y
 	git clone https://github.com/robisonsantos/evm
 	cd evm && ./install
 	echo 'source $HOME/.evm/scripts/evm' > ~/.bashrc
