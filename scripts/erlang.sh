8165649d09336958937b78b96e69cf4d30142a2d
eabz <eabz@polispay.org>
2021-08-01 22:04:23 -0500

    gixes

--- scripts/erlang.sh
+++ scripts/erlang.sh
@@ -17,7 +17,7 @@ fi
 
 function install() {
   echo "Installing Erlang..."
-  apt install openssl libssl-dev fop xsltproc unixodbc-dev libxml2-utils libwxbase3.0-0v5 libwxbase3.0-dev libqt5opengl5-dev libncurses-dev libwxgtk3-3.0-gtk3-dev libwxgtk3-3.0-gtk3-0v5 wx-common -y
+  apt install openssl libssl-dev fop xsltproc unixodbc-dev libxml2-utils libwxbase3.0-0v5 libwxbase3.0-dev libqt5opengl5-dev libncurses-dev wx-common -y
 	git clone https://github.com/robisonsantos/evm
 	cd evm && ./install
 	echo 'source $HOME/.evm/scripts/evm' > ~/.bashrc
