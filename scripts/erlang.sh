fe14fcc83e8770cda50029e9d7aaee772e33d635
eabz <eabz@polispay.org>
2021-08-01 21:59:05 -0500

    Install evm

---
+++ scripts/erlang.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+function check() {
+if [ $? -eq 0 ]
+then
+    erl --version | grep "cargo" &> /dev/null
+    if [ $? -eq 0 ]
+    then
+        echo "Rust is already installed"
+    else
+        install
+    fi
+else
+    install
+fi
+}
+
+function install() {
+  echo "Installing Erlang..."
+	git clone https://github.com/robisonsantos/evm
+	cd evm && ./install
+	echo 'source $HOME/.evm/scripts/evm' > ~/.bashrc
+	. "$HOME"/.evm/scripts/evm
+	evm install 23.3.4.1
+}
+
+check
