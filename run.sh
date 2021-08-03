20bcfd99b6af7e3a84b356067b63577c9302e347
eabz <eabz@polispay.org>
2021-08-02 19:41:08 -0500

    Fix for relative paths

--- run.sh
+++ run.sh
@@ -8,10 +8,10 @@ function run_sparta_simple() {
     echo "==> Starting Polis node on SPARTA network"
   	docker run -d \
 		-e NETHERMIND_CONFIG=sparta \
-		-v /root/polis-chains/configs/sparta/static-nodes.json:/nethermind/Data/static-nodes.json \
- 		-v /root/nethermind_db/:/nethermind/nethermind_db/ \
- 		-v /root/logs/:/nethermind/logs/ \
- 		-e NETHERMIND_MININGCONFIG_MINGASPRICE="1000000000" \
+		-e NETHERMIND_MININGCONFIG_MINGASPRICE="1000000000" \
+		-v "`pwd`"/configs/sparta/static-nodes.json:/nethermind/Data/static-nodes.json \
+ 		-v "`pwd`"/nethermind_db/:/nethermind/nethermind_db/ \
+ 		-v "`pwd`"/logs/:/nethermind/logs/ \
  		ghcr.io/polischain/polis-chains:main
 }
 
@@ -26,9 +26,9 @@ function run_sparta_rpc() {
 		-e NETHERMIND_JSONRPCCONFIG_HOST=0.0.0.0 \
 		-p 8545:8545 \
 		-p 8546:8546 \
-		-v /root/polis-chains/configs/sparta/static-nodes.json:/nethermind/Data/static-nodes.json \
- 		-v /root/nethermind_db/:/nethermind/nethermind_db/ \
- 		-v /root/logs/:/nethermind/logs/ \
+		-v "`pwd`"/configs/sparta/static-nodes.json:/nethermind/Data/static-nodes.json \
+ 		-v "`pwd`"/nethermind_db/:/nethermind/nethermind_db/ \
+ 		-v "`pwd`"/logs/:/nethermind/logs/ \
  		ghcr.io/polischain/polis-chains:main
 }
 
@@ -44,11 +44,11 @@ function run_sparta_validator() {
 		-e NETHERMIND_KEYSTORECONFIG_BLOCKAUTHORACCOUNT="$ACCOUNT" \
 		-e NETHERMIND_KEYSTORECONFIG_UNLOCKACCOUNTS="$ACCOUNT" \
 		-e NETHERMIND_KEYSTORECONFIG_PASSWORDFILES=/nethermind/passwords/node.pwd \
-		-v /root/polis-chains/configs/sparta/static-nodes.json:/nethermind/Data/static-nodes.json \
-		-v /root/passwords/:/nethermind/passwords/ \
- 		-v /root/nethermind_db/:/nethermind/nethermind_db/ \
- 		-v /root/keystore/:/nethermind/keystore \
- 		-v /root/logs/:/nethermind/logs/ \
+		-v "`pwd`"/configs/sparta/static-nodes.json:/nethermind/Data/static-nodes.json \
+		-v "`pwd`"/passwords/:/nethermind/passwords/ \
+ 		-v "`pwd`"/nethermind_db/:/nethermind/nethermind_db/ \
+ 		-v "`pwd`"/keystore/:/nethermind/keystore \
+ 		-v "`pwd`"/logs/:/nethermind/logs/ \
  		ghcr.io/polischain/polis-chains:main
 }
 
