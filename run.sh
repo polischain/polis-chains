cc809219ee8aff961b8e3bafbf9753f5aee4d690
eabz <eabz@polispay.org>
2021-08-10 22:13:42 -0500

    Prettify

--- run.sh
+++ run.sh
@@ -5,122 +5,122 @@ TYPE=$2
 ACCOUNT=$3
 
 function run_sparta_simple() {
-    echo "==> Starting Polis node on SPARTA network"
-  	docker run -d --restart=always \
-  	-p 30303:30303 \
-  	-p 30303:30303/udp \
-  	-e NETHERMIND_HEALTHCHECKSCONFIG_ENABLED="true" \
-   	-e NETHERMIND_HEALTHCHECKSCONFIG_WEBHOOKSENABLED="true" \
-   	-e NETHERMIND_HEALTHCHECKSCONFIG_UIENABLED="true" \
-		-e NETHERMIND_CONFIG=sparta \
-		-e NETHERMIND_ETHSTATSCONFIG_ENABLED="true" \
-		-e NETHERMIND_ETHSTATSCONFIG_SECRET="PhD8zsx69jhpqv7PUhzz2mExj66hT8tRknP7Rw7sCC5y79SAgQuZLW6cXUuqjRnv" \
-		-e NETHERMIND_ETHSTATSCONFIG_SERVER="wss://sparta-stats.polis.tech/api" \
-		-e NETHERMIND_ETHSTATSCONFIG_NAME="$NAME" \
-		-e NETHERMIND_MININGCONFIG_MINGASPRICE="1000000000" \
- 		-v "$(pwd)"/db/:/nethermind/nethermind_db/ \
- 		-v "$(pwd)"/keystore/:/nethermind/keystore/ \
- 		-v "$(pwd)"/logs/:/nethermind/logs/ \
- 		ghcr.io/polischain/polis-chains:main
+  echo "==> Starting Polis node on SPARTA network"
+  docker run -d --restart=always \
+    -p 30303:30303 \
+    -p 30303:30303/udp \
+    -e NETHERMIND_HEALTHCHECKSCONFIG_ENABLED="true" \
+    -e NETHERMIND_HEALTHCHECKSCONFIG_WEBHOOKSENABLED="true" \
+    -e NETHERMIND_HEALTHCHECKSCONFIG_UIENABLED="true" \
+    -e NETHERMIND_CONFIG=sparta \
+    -e NETHERMIND_ETHSTATSCONFIG_ENABLED="true" \
+    -e NETHERMIND_ETHSTATSCONFIG_SECRET="PhD8zsx69jhpqv7PUhzz2mExj66hT8tRknP7Rw7sCC5y79SAgQuZLW6cXUuqjRnv" \
+    -e NETHERMIND_ETHSTATSCONFIG_SERVER="wss://sparta-stats.polis.tech/api" \
+    -e NETHERMIND_ETHSTATSCONFIG_NAME="$NAME" \
+    -e NETHERMIND_MININGCONFIG_MINGASPRICE="1000000000" \
+    -v "$(pwd)"/db/:/nethermind/nethermind_db/ \
+    -v "$(pwd)"/keystore/:/nethermind/keystore/ \
+    -v "$(pwd)"/logs/:/nethermind/logs/ \
+    ghcr.io/polischain/polis-chains:main
 }
 
 function run_olympus_simple() {
-    echo "==> Starting Polis node on OLYMPUS network"
-  	docker run -d --restart=always \
-  	-p 30303:30303 \
-  	-p 30303:30303/udp \
-		-e NETHERMIND_CONFIG=olympus \
-		-e NETHERMIND_MININGCONFIG_MINGASPRICE="1000000000" \
- 		-v "$(pwd)"/db/:/nethermind/nethermind_db/ \
- 		-v "$(pwd)"/keystore/:/nethermind/keystore/ \
- 		-v "$(pwd)"/logs/:/nethermind/logs/ \
- 		ghcr.io/polischain/polis-chains:main
+  echo "==> Starting Polis node on OLYMPUS network"
+  docker run -d --restart=always \
+    -p 30303:30303 \
+    -p 30303:30303/udp \
+    -e NETHERMIND_CONFIG=olympus \
+    -e NETHERMIND_MININGCONFIG_MINGASPRICE="1000000000" \
+    -v "$(pwd)"/db/:/nethermind/nethermind_db/ \
+    -v "$(pwd)"/keystore/:/nethermind/keystore/ \
+    -v "$(pwd)"/logs/:/nethermind/logs/ \
+    ghcr.io/polischain/polis-chains:main
 }
 
 function run_sparta_rpc() {
   echo "==> Starting Polis node on SPARTA network and JSON RPC exposed"
   docker run -d --restart=always \
-  	-p 30303:30303 \
-  	-p 30303:30303/udp \
-		-e NETHERMIND_CONFIG=sparta \
-		-e NETHERMIND_ETHSTATSCONFIG_ENABLED="true" \
-		-e NETHERMIND_ETHSTATSCONFIG_SECRET="PhD8zsx69jhpqv7PUhzz2mExj66hT8tRknP7Rw7sCC5y79SAgQuZLW6cXUuqjRnv" \
-		-e NETHERMIND_ETHSTATSCONFIG_SERVER="wss://sparta-stats.polis.tech/api" \
-		-e NETHERMIND_ETHSTATSCONFIG_NAME="$NAME" \
-		-e NETHERMIND_INITCONFIG_WEBSOCKETSENABLED=true \
-		-e NETHERMIND_JSONRPCCONFIG_WEBSOCKETSPORT=8546 \
-		-e NETHERMIND_JSONRPCCONFIG_ENABLEDMODULES=eth,subscribe,trace,txpool,web3,proof,net,parity,health \
-		-e NETHERMIND_JSONRPCCONFIG_ENABLED=true \
-		-e NETHERMIND_JSONRPCCONFIG_HOST=0.0.0.0 \
-		-p 8545:8545 \
-		-p 8546:8546 \
- 		-v "$(pwd)"/db/:/nethermind/nethermind_db/ \
- 		-v "$(pwd)"/keystore/:/nethermind/keystore/ \
- 		-v "$(pwd)"/logs/:/nethermind/logs/ \
- 		ghcr.io/polischain/polis-chains:main
+    -p 30303:30303 \
+    -p 30303:30303/udp \
+    -e NETHERMIND_CONFIG=sparta \
+    -e NETHERMIND_ETHSTATSCONFIG_ENABLED="true" \
+    -e NETHERMIND_ETHSTATSCONFIG_SECRET="PhD8zsx69jhpqv7PUhzz2mExj66hT8tRknP7Rw7sCC5y79SAgQuZLW6cXUuqjRnv" \
+    -e NETHERMIND_ETHSTATSCONFIG_SERVER="wss://sparta-stats.polis.tech/api" \
+    -e NETHERMIND_ETHSTATSCONFIG_NAME="$NAME" \
+    -e NETHERMIND_INITCONFIG_WEBSOCKETSENABLED=true \
+    -e NETHERMIND_JSONRPCCONFIG_WEBSOCKETSPORT=8546 \
+    -e NETHERMIND_JSONRPCCONFIG_ENABLEDMODULES=eth,subscribe,trace,txpool,web3,proof,net,parity,health \
+    -e NETHERMIND_JSONRPCCONFIG_ENABLED=true \
+    -e NETHERMIND_JSONRPCCONFIG_HOST=0.0.0.0 \
+    -p 8545:8545 \
+    -p 8546:8546 \
+    -v "$(pwd)"/db/:/nethermind/nethermind_db/ \
+    -v "$(pwd)"/keystore/:/nethermind/keystore/ \
+    -v "$(pwd)"/logs/:/nethermind/logs/ \
+    ghcr.io/polischain/polis-chains:main
 }
 
 function run_olympus_rpc() {
   echo "==> Starting Polis node on OLYMPUS network and JSON RPC exposed"
   docker run -d --restart=always \
-  	-p 30303:30303 \
-  	-p 30303:30303/udp \
-		-e NETHERMIND_CONFIG=olympus \
-		-e NETHERMIND_INITCONFIG_WEBSOCKETSENABLED=true \
-		-e NETHERMIND_JSONRPCCONFIG_WEBSOCKETSPORT=8546 \
-		-e NETHERMIND_JSONRPCCONFIG_ENABLEDMODULES=eth,subscribe,trace,txpool,web3,proof,net,parity,health \
-		-e NETHERMIND_JSONRPCCONFIG_ENABLED=true \
-		-e NETHERMIND_JSONRPCCONFIG_HOST=0.0.0.0 \
-		-p 8545:8545 \
-		-p 8546:8546 \
- 		-v "$(pwd)"/db/:/nethermind/nethermind_db/ \
- 		-v "$(pwd)"/keystore/:/nethermind/keystore/ \
- 		-v "$(pwd)"/logs/:/nethermind/logs/ \
- 		ghcr.io/polischain/polis-chains:main
+    -p 30303:30303 \
+    -p 30303:30303/udp \
+    -e NETHERMIND_CONFIG=olympus \
+    -e NETHERMIND_INITCONFIG_WEBSOCKETSENABLED=true \
+    -e NETHERMIND_JSONRPCCONFIG_WEBSOCKETSPORT=8546 \
+    -e NETHERMIND_JSONRPCCONFIG_ENABLEDMODULES=eth,subscribe,trace,txpool,web3,proof,net,parity,health \
+    -e NETHERMIND_JSONRPCCONFIG_ENABLED=true \
+    -e NETHERMIND_JSONRPCCONFIG_HOST=0.0.0.0 \
+    -p 8545:8545 \
+    -p 8546:8546 \
+    -v "$(pwd)"/db/:/nethermind/nethermind_db/ \
+    -v "$(pwd)"/keystore/:/nethermind/keystore/ \
+    -v "$(pwd)"/logs/:/nethermind/logs/ \
+    ghcr.io/polischain/polis-chains:main
 }
 
 function run_sparta_validator() {
   echo "==> Starting Polis node on SPARTA network and enabled for mining"
 	docker run -d --restart=always \
-  	-p 30303:30303 \
-  	-p 30303:30303/udp \
-		-e NETHERMIND_CONFIG=sparta \
-		-e NETHERMIND_ETHSTATSCONFIG_ENABLED="true" \
-		-e NETHERMIND_ETHSTATSCONFIG_SECRET="PhD8zsx69jhpqv7PUhzz2mExj66hT8tRknP7Rw7sCC5y79SAgQuZLW6cXUuqjRnv" \
-		-e NETHERMIND_ETHSTATSCONFIG_SERVER="wss://sparta-stats.polis.tech/api" \
-		-e NETHERMIND_ETHSTATSCONFIG_NAME="$NAME" \
-		-e NETHERMIND_INITCONFIG_ISMINING="true" \
-		-e NETHERMIND_MININGCONFIG_ENABLED="true" \
-		-e NETHERMIND_MININGCONFIG_MINGASPRICE="1000000000" \
-		-e NETHERMIND_MININGCONFIG_TARGETBLOCKGASLIMIT="20000000" \
-		-e NETHERMIND_KEYSTORECONFIG_BLOCKAUTHORACCOUNT="$ACCOUNT" \
-		-e NETHERMIND_KEYSTORECONFIG_UNLOCKACCOUNTS="$ACCOUNT" \
-		-e NETHERMIND_KEYSTORECONFIG_PASSWORDFILES=/nethermind/passwords/"$ACCOUNT" \
-		-v "$(pwd)"/passwords/:/nethermind/passwords/ \
- 		-v "$(pwd)"/db/:/nethermind/nethermind_db/ \
- 		-v "$(pwd)"/keystore/:/nethermind/keystore \
- 		-v "$(pwd)"/logs/:/nethermind/logs/ \
- 		ghcr.io/polischain/polis-chains:main
+    -p 30303:30303 \
+    -p 30303:30303/udp \
+    -e NETHERMIND_CONFIG=sparta \
+    -e NETHERMIND_ETHSTATSCONFIG_ENABLED="true" \
+    -e NETHERMIND_ETHSTATSCONFIG_SECRET="PhD8zsx69jhpqv7PUhzz2mExj66hT8tRknP7Rw7sCC5y79SAgQuZLW6cXUuqjRnv" \
+    -e NETHERMIND_ETHSTATSCONFIG_SERVER="wss://sparta-stats.polis.tech/api" \
+    -e NETHERMIND_ETHSTATSCONFIG_NAME="$NAME" \
+    -e NETHERMIND_INITCONFIG_ISMINING="true" \
+    -e NETHERMIND_MININGCONFIG_ENABLED="true" \
+    -e NETHERMIND_MININGCONFIG_MINGASPRICE="1000000000" \
+    -e NETHERMIND_MININGCONFIG_TARGETBLOCKGASLIMIT="20000000" \
+    -e NETHERMIND_KEYSTORECONFIG_BLOCKAUTHORACCOUNT="$ACCOUNT" \
+    -e NETHERMIND_KEYSTORECONFIG_UNLOCKACCOUNTS="$ACCOUNT" \
+    -e NETHERMIND_KEYSTORECONFIG_PASSWORDFILES=/nethermind/passwords/"$ACCOUNT" \
+    -v "$(pwd)"/passwords/:/nethermind/passwords/ \
+    -v "$(pwd)"/db/:/nethermind/nethermind_db/ \
+    -v "$(pwd)"/keystore/:/nethermind/keystore \
+    -v "$(pwd)"/logs/:/nethermind/logs/ \
+    ghcr.io/polischain/polis-chains:main
 }
 
 function run_olympus_validator() {
   echo "==> Starting Polis node on OLYMPUS network and enabled for mining"
 	docker run -d --restart=always \
-  	-p 30303:30303 \
-  	-p 30303:30303/udp \
-		-e NETHERMIND_CONFIG=olympus \
-		-e NETHERMIND_INITCONFIG_ISMINING="true" \
-		-e NETHERMIND_MININGCONFIG_ENABLED="true" \
-		-e NETHERMIND_MININGCONFIG_MINGASPRICE="1000000000" \
-		-e NETHERMIND_MININGCONFIG_TARGETBLOCKGASLIMIT="20000000" \
-		-e NETHERMIND_KEYSTORECONFIG_BLOCKAUTHORACCOUNT="$ACCOUNT" \
-		-e NETHERMIND_KEYSTORECONFIG_UNLOCKACCOUNTS="$ACCOUNT" \
-		-e NETHERMIND_KEYSTORECONFIG_PASSWORDFILES=/nethermind/passwords/"$ACCOUNT" \
-		-v "$(pwd)"/passwords/:/nethermind/passwords/ \
- 		-v "$(pwd)"/db/:/nethermind/nethermind_db/ \
- 		-v "$(pwd)"/keystore/:/nethermind/keystore \
- 		-v "$(pwd)"/logs/:/nethermind/logs/ \
- 		ghcr.io/polischain/polis-chains:main
+    -p 30303:30303 \
+    -p 30303:30303/udp \
+    -e NETHERMIND_CONFIG=olympus \
+    -e NETHERMIND_INITCONFIG_ISMINING="true" \
+    -e NETHERMIND_MININGCONFIG_ENABLED="true" \
+    -e NETHERMIND_MININGCONFIG_MINGASPRICE="1000000000" \
+    -e NETHERMIND_MININGCONFIG_TARGETBLOCKGASLIMIT="20000000" \
+    -e NETHERMIND_KEYSTORECONFIG_BLOCKAUTHORACCOUNT="$ACCOUNT" \
+    -e NETHERMIND_KEYSTORECONFIG_UNLOCKACCOUNTS="$ACCOUNT" \
+    -e NETHERMIND_KEYSTORECONFIG_PASSWORDFILES=/nethermind/passwords/"$ACCOUNT" \
+    -v "$(pwd)"/passwords/:/nethermind/passwords/ \
+    -v "$(pwd)"/db/:/nethermind/nethermind_db/ \
+    -v "$(pwd)"/keystore/:/nethermind/keystore \
+    -v "$(pwd)"/logs/:/nethermind/logs/ \
+    ghcr.io/polischain/polis-chains:main
 }
 
 function run_olympus() {
