43d45866b07efa41aca585b38d129043590008a0
eabz <eabz@polispay.org>
2021-08-23 16:29:02 -0500

    Simplify

--- run.sh
+++ run.sh
@@ -4,23 +4,6 @@ NETWORK=$1
 TYPE=$2
 ACCOUNT=$3
 
-function run_sparta_simple_archived() {
-  echo "==> Starting Polis node on SPARTA network"
-  docker run -d --restart=always \
-    -p 30303:30303 \
-    -p 30303:30303/udp \
-    -e NETHERMIND_CONFIG=sparta_archive \
-    -e NETHERMIND_ETHSTATSCONFIG_ENABLED="true" \
-    -e NETHERMIND_ETHSTATSCONFIG_SECRET="PhD8zsx69jhpqv7PUhzz2mExj66hT8tRknP7Rw7sCC5y79SAgQuZLW6cXUuqjRnv" \
-    -e NETHERMIND_ETHSTATSCONFIG_SERVER="wss://sparta-stats.polis.tech/api" \
-    -e NETHERMIND_ETHSTATSCONFIG_NAME="$NAME" \
-    -e NETHERMIND_MININGCONFIG_MINGASPRICE="1000000000" \
-    -v "$(pwd)"/db/:/nethermind/nethermind_db/ \
-    -v "$(pwd)"/keystore/:/nethermind/keystore/ \
-    -v "$(pwd)"/logs/:/nethermind/logs/ \
-    ghcr.io/polischain/polis-chains:main
-}
-
 function run_sparta_simple() {
   echo "==> Starting Polis node on SPARTA network"
   docker run -d --restart=always \
@@ -61,29 +44,6 @@ function run_sparta_rpc() {
     ghcr.io/polischain/polis-chains:main
 }
 
-function run_sparta_rpc_internal() {
-  echo "==> Starting Polis node on SPARTA network and JSON RPC exposed"
-  docker run -d --restart=always \
-    -p 30303:30303 \
-    -p 30303:30303/udp \
-    -e NETHERMIND_CONFIG=sparta \
-    -e NETHERMIND_ETHSTATSCONFIG_ENABLED="true" \
-    -e NETHERMIND_ETHSTATSCONFIG_SECRET="PhD8zsx69jhpqv7PUhzz2mExj66hT8tRknP7Rw7sCC5y79SAgQuZLW6cXUuqjRnv" \
-    -e NETHERMIND_ETHSTATSCONFIG_SERVER="wss://sparta-stats.polis.tech/api" \
-    -e NETHERMIND_ETHSTATSCONFIG_NAME="$NAME" \
-    -e NETHERMIND_INITCONFIG_WEBSOCKETSENABLED=true \
-    -e NETHERMIND_JSONRPCCONFIG_WEBSOCKETSPORT=8546 \
-    -e NETHERMIND_JSONRPCCONFIG_ENABLEDMODULES=eth,subscribe,trace,txpool,web3,proof,net,parity,health \
-    -e NETHERMIND_JSONRPCCONFIG_ENABLED=true \
-    -e NETHERMIND_JSONRPCCONFIG_HOST=127.0.0.1 \
-    -p 8545:8545 \
-    -p 8546:8546 \
-    -v "$(pwd)"/db/:/nethermind/nethermind_db/ \
-    -v "$(pwd)"/keystore/:/nethermind/keystore/ \
-    -v "$(pwd)"/logs/:/nethermind/logs/ \
-    ghcr.io/polischain/polis-chains:main
-}
-
 function run_sparta_validator() {
   echo "==> Starting Polis node on SPARTA network and enabled for mining"
 	docker run -d --restart=always \
@@ -116,12 +76,6 @@ case "$TYPE" in
   bash scripts/docker.sh &> /dev/null
   run_sparta_rpc
 ;;
-"rpc_internal")
-  echo "==> Running a node for SPARTA configured with internal RPC"
-  echo "==> Checking docker installation..."
-  bash scripts/docker.sh &> /dev/null
-  run_sparta_rpc_internal
-;;
 "validator")
   echo "==> Running a node for SPARTA configured with validator configuration"
   echo "==> Checking docker installation..."
@@ -138,12 +92,6 @@ case "$TYPE" in
   echo "==> Checking docker installation..."
   bash scripts/docker.sh &> /dev/null
   run_sparta_simple
-;;
-"node_archived")
-  echo "==> Running a simple node for SPARTA"
-  echo "==> Checking docker installation..."
-  bash scripts/docker.sh &> /dev/null
-  run_sparta_simple_archived
 ;;
 *)
 echo "Unknown configuration type for SPARTA please specify a node setup: rpc, explorer, validator, node"
