26472b65e30b5532561eef2e1d5e5cb0c536670d
eabz <eabz@polispay.org>
2021-08-28 21:26:02 -0500

    Change rpc to openethereum

--- run.sh
+++ run.sh
@@ -21,29 +21,6 @@ function run_sparta_simple() {
     ghcr.io/polischain/polis-chains:main
 }
 
-function run_sparta_rpc() {
-  echo "==> Starting Polis node on SPARTA network and JSON RPC exposed"
-  docker run -d --restart=always \
-    -p 30303:30303 \
-    -p 30303:30303/udp \
-    -e NETHERMIND_CONFIG=sparta \
-    -e NETHERMIND_ETHSTATSCONFIG_ENABLED="true" \
-    -e NETHERMIND_ETHSTATSCONFIG_SECRET="PhD8zsx69jhpqv7PUhzz2mExj66hT8tRknP7Rw7sCC5y79SAgQuZLW6cXUuqjRnv" \
-    -e NETHERMIND_ETHSTATSCONFIG_SERVER="wss://sparta-stats.polis.tech/api" \
-    -e NETHERMIND_ETHSTATSCONFIG_NAME="$NAME" \
-    -e NETHERMIND_INITCONFIG_WEBSOCKETSENABLED=true \
-    -e NETHERMIND_JSONRPCCONFIG_WEBSOCKETSPORT=8546 \
-    -e NETHERMIND_JSONRPCCONFIG_ENABLEDMODULES=eth,subscribe,trace,txpool,web3,proof,net,parity,health \
-    -e NETHERMIND_JSONRPCCONFIG_ENABLED=true \
-    -e NETHERMIND_JSONRPCCONFIG_HOST=0.0.0.0 \
-    -p 8545:8545 \
-    -p 8546:8546 \
-    -v "$(pwd)"/db/:/nethermind/nethermind_db/ \
-    -v "$(pwd)"/keystore/:/nethermind/keystore/ \
-    -v "$(pwd)"/logs/:/nethermind/logs/ \
-    ghcr.io/polischain/polis-chains:main
-}
-
 function run_sparta_validator() {
   echo "==> Starting Polis node on SPARTA network and enabled for mining"
 	docker run -d --restart=always \
@@ -70,12 +47,6 @@ function run_sparta_validator() {
 
 function run_sparta() {
 case "$TYPE" in
-"rpc")
-  echo "==> Running a node for SPARTA configured with exposed RPC"
-  echo "==> Checking docker installation..."
-  bash scripts/docker.sh &> /dev/null
-  run_sparta_rpc
-;;
 "validator")
   echo "==> Running a node for SPARTA configured with validator configuration"
   echo "==> Checking docker installation..."
